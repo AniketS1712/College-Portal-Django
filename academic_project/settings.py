00d876a8c773cff841c28acee53f054cf88bceeb
Aniket <singhalaniket17@gmail.com>
2024-12-21 20:19:11 +0530

    Deploying on Render commit

--- academic_project/settings.py
+++ academic_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-29g+xsf71q+#k=w&&y%1m)qar(1p)2w=4k697oforgdq^*25-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
